67e05237bab465c79bc878f4dea8605f9a278dbe
Jhorman Orozco <jsojhor@gmail.com>
2022-08-18 23:38:03 -0500

    updae image

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
+import { PDF } from './PDF';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const doc = new jsPDF();
+
+  const handleClick = () => {
+    const input = document.getElementById('divToPrint');
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      console.log(imgData);
+      doc.addImage(imgData, 'JPEG', 0, 0, 200, 300);
+      doc.save('test.pdf');
+    });
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PDF />
+      <button onClick={handleClick}>Print</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
